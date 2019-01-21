95afbe2f119148fae75551591040466689bf7ee1
muryno <github.com/muryno>
2019-01-21 17:28:36 +0100

    Initial commit

---
+++ app/src/main/java/connectrail/com/floatingactionbuttonanimation/MainActivity.java
@@ -0,0 +1,151 @@
+package connectrail.com.floatingactionbuttonanimation;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    FloatingActionButton fab;
+    FloatingActionButton fab1;
+    FloatingActionButton fab2;
+    FloatingActionButton fab3;
+    FloatingActionButton fab4;
+
+    //Save the FAB's active status
+    //false -> fab = close
+    //true -> fab = open
+    private boolean FAB_Status = false;
+
+    //Animations
+    Animation show_fab_1;
+    Animation hide_fab_1;
+    Animation show_fab_2;
+    Animation hide_fab_2;
+    Animation show_fab_3;
+    Animation hide_fab_3;
+    Animation show_fab_4;
+    Animation hide_fab_4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab1 = findViewById(R.id.fab_1);
+        fab2 = findViewById(R.id.fab_2);
+        fab3 = findViewById(R.id.fab_3);
+        fab4 = findViewById(R.id.fab_4);
+
+        //Animations
+        show_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_show);
+        hide_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_hide);
+        show_fab_2 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab2_show);
+        hide_fab_2 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab2_hide);
+        show_fab_3 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab3_show);
+        hide_fab_3 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab3_hide);
+        show_fab_4 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab4_show);
+        hide_fab_4 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab4_hide);
+
+
+        fab.setOnClickListener(view -> {
+
+            if (!FAB_Status) {
+                //Display FAB menu
+
+                expandFAB();
+                FAB_Status = true;
+            } else {
+               
+                //Close FAB menu
+                hideFAB();
+                FAB_Status = false;
+            }
+        });
+
+        fab1.setOnClickListener(v -> Toast.makeText(getApplication(), "Floating Action Button 1", Toast.LENGTH_SHORT).show());
+
+        fab2.setOnClickListener(v -> Toast.makeText(getApplication(), "Floating Action Button 2", Toast.LENGTH_SHORT).show());
+
+        fab4.setOnClickListener(v -> Toast.makeText(getApplication(), "Floating Action Button 4", Toast.LENGTH_SHORT).show());
+        fab3.setOnClickListener(v -> Toast.makeText(getApplication(), "Floating Action Button 3", Toast.LENGTH_SHORT).show());
+    }
+
+    private void expandFAB() {
+
+        //Floating Action Button 1
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+        layoutParams.rightMargin += (int) (fab1.getWidth() * 1.7);
+        layoutParams.bottomMargin += (int) (fab1.getHeight() * 0.25);
+        fab1.setLayoutParams(layoutParams);
+        fab1.startAnimation(show_fab_1);
+        fab1.setClickable(true);
+
+        //Floating Action Button 2
+        FrameLayout.LayoutParams layoutParams2 = (FrameLayout.LayoutParams) fab2.getLayoutParams();
+        layoutParams2.rightMargin += (int) (fab2.getWidth() * 1.5);
+        layoutParams2.bottomMargin += (int) (fab2.getHeight() * 1.5);
+        fab2.setLayoutParams(layoutParams2);
+        fab2.startAnimation(show_fab_2);
+        fab2.setClickable(true);
+
+        //Floating Action Button 3
+        FrameLayout.LayoutParams layoutParams3 = (FrameLayout.LayoutParams) fab3.getLayoutParams();
+        layoutParams3.rightMargin += (int) (fab3.getWidth() * 0.25);
+        layoutParams3.bottomMargin += (int) (fab3.getHeight() * 1.7);
+        fab3.setLayoutParams(layoutParams3);
+        fab3.startAnimation(show_fab_3);
+        fab3.setClickable(true);
+
+        //Floating Action Button 3
+        FrameLayout.LayoutParams layoutParams4 = (FrameLayout.LayoutParams) fab4.getLayoutParams();
+        layoutParams4.rightMargin += (int) (fab4.getWidth() * 0.25);
+        layoutParams4.bottomMargin += (int) (fab4.getHeight() * 1.7);
+        fab4.setLayoutParams(layoutParams4);
+        fab4.startAnimation(show_fab_4);
+        fab4.setClickable(true);
+    }
+
+
+    private void hideFAB() {
+
+        //Floating Action Button 1
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+        layoutParams.rightMargin -= (int) (fab1.getWidth() * 1.7);
+        layoutParams.bottomMargin -= (int) (fab1.getHeight() * 0.25);
+        fab1.setLayoutParams(layoutParams);
+        fab1.startAnimation(hide_fab_1);
+        fab1.setClickable(false);
+
+        //Floating Action Button 2
+        FrameLayout.LayoutParams layoutParams2 = (FrameLayout.LayoutParams) fab2.getLayoutParams();
+        layoutParams2.rightMargin -= (int) (fab2.getWidth() * 1.5);
+        layoutParams2.bottomMargin -= (int) (fab2.getHeight() * 1.5);
+        fab2.setLayoutParams(layoutParams2);
+        fab2.startAnimation(hide_fab_2);
+        fab2.setClickable(false);
+
+        //Floating Action Button 3
+        FrameLayout.LayoutParams layoutParams3 = (FrameLayout.LayoutParams) fab3.getLayoutParams();
+        layoutParams3.rightMargin -= (int) (fab3.getWidth() * 0.25);
+        layoutParams3.bottomMargin -= (int) (fab3.getHeight() * 1.7);
+        fab3.setLayoutParams(layoutParams3);
+        fab3.startAnimation(hide_fab_3);
+        fab3.setClickable(false);
+
+        //Floating Action Button 4
+        FrameLayout.LayoutParams layoutParams4 = (FrameLayout.LayoutParams) fab4.getLayoutParams();
+        layoutParams4.rightMargin -= (int) (fab4.getWidth() * 0.25);
+        layoutParams4.bottomMargin -= (int) (fab4.getHeight() * 1.7);
+        fab4.setLayoutParams(layoutParams4);
+        fab4.startAnimation(hide_fab_4);
+        fab4.setClickable(false);
+    }
+}
